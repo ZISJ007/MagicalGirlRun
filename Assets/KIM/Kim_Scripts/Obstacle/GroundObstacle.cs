ada4dd6dd31de6433ccb01c839a9b5fc1ddc53bf
KimgiChan07 <wekorean0727@naver.com>
2025-05-09 11:31:16 +0900

    refactor: 스크립트 메서드 수정, feat: BaseObstacle제작

---
+++ Assets/KIM/Kim_Scripts/Obstacle/GroundObstacle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundObstacle : BaseObstacleSpawner
+{
+
+    public override void Spawn()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+}
