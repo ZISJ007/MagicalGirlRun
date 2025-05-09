d98f5bed4a3da4ea610997d8ab69c036be033742
KimgiChan07 <wekorean0727@naver.com>
2025-05-09 16:42:56 +0900

    refactor: 공중장애물 떨어지는 시스템 제작

--- Assets/KIM/Kim_Scripts/Obstacle/AerialObstacle.cs
+++ Assets/KIM/Kim_Scripts/Obstacle/AerialObstacle.cs
@@ -2,16 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AerialObstacle : BaseObstacleSpawner
+public class AerialObstacle : BaseObstacle
 {
+    [SerializeField] private float downMoveSpeed;
+    [SerializeField] private float targetY;
+    private bool isYMoving=false;
 
     public override void Spawn()
     {
-        throw new System.NotImplementedException();
+        
     }
 
-    public override void Update()
+    protected override void Update()
     {
-        throw new System.NotImplementedException();
+       base.Update();
+       if (isYMoving)
+       {
+           YMove();
+       }
+    }
+
+    protected override void YMove()
+    {
+        if (transform.position.y > targetY)
+        {
+            transform.position += Vector3.down * downMoveSpeed * Time.deltaTime;
+            if (transform.position.y < targetY)
+            {
+                transform.position =new Vector3(transform.position.x, targetY, transform.position.z);;
+                isYMoving = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("YMoveTrigger"))
+        {
+            isYMoving = true;
+        }
     }
 }
