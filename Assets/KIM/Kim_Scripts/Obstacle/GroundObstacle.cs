47dd3ab6e92bd2402e9f89f1e7a79bc5a9abe91f
KimgiChan07 <wekorean0727@naver.com>
2025-05-09 16:44:36 +0900

    refactor: 지상장애물 이동스크립트 제작

--- Assets/KIM/Kim_Scripts/Obstacle/GroundObstacle.cs
+++ Assets/KIM/Kim_Scripts/Obstacle/GroundObstacle.cs
@@ -1,17 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GroundObstacle : BaseObstacleSpawner
+public class GroundObstacle : BaseObstacle
 {
+    [SerializeField] private float upMoveSpeed;
+    [SerializeField] private float targetY;
+    
+    private bool isYMoving = false;
 
     public override void Spawn()
     {
-        throw new System.NotImplementedException();
     }
 
-    public override void Update()
+    protected override void Update()
     {
-        throw new System.NotImplementedException();
+        base.Update();
+        if (isYMoving)
+        {
+            YMove();
+        }
+    }
+
+    protected override void YMove()
+    {
+        if (transform.position.y < targetY)
+        {
+            transform.position += Vector3.up * upMoveSpeed * Time.deltaTime;
+            if (transform.position.y > targetY)
+            {
+                transform.position =new Vector3(transform.position.x, targetY, transform.position.z);;
+                isYMoving = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("YMoveTrigger"))
+        {
+            isYMoving = true;
+        }
     }
 }
