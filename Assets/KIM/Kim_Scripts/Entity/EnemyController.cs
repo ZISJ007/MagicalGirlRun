23a3c4bf69f8d221cbf0c2852f61f235061df231
KimgiChan07 <wekorean0727@naver.com>
2025-05-13 11:05:00 +0900

    feat: 장애물 추가, refactor: 애니메이션 수정

--- Assets/KIM/Kim_Scripts/Entity/EnemyController.cs
+++ Assets/KIM/Kim_Scripts/Entity/EnemyController.cs
@@ -32,13 +32,13 @@ public class EnemyController : MonoBehaviour
 
     private GameObject groundAttackObject;
     //애니메이션
-    private static readonly int isAttack = Animator.StringToHash("IsAttack");
-    private static readonly int isAttackReady = Animator.StringToHash("IsAttackReady");
+    private static readonly int isGroundAttack = Animator.StringToHash("GroundIsAttack");
+    private static readonly int isAerialAttack = Animator.StringToHash("AerialIsAttack");
     private Animator animator;
 
     private void Awake()
     {
-            animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
         poolManager = FindObjectOfType<PoolManager>();
         if (playerTransform == null) return;
         if (groundAttackObject == null) return;
@@ -65,7 +65,7 @@ public class EnemyController : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(2f);
-            AttackAnimation();
+            GroundAttackAnimation();
             yield return new WaitForSeconds(groundAttackInterval);
         }
     }
@@ -74,6 +74,8 @@ public class EnemyController : MonoBehaviour
     {
         while (true)
         {
+            AerialAttackAnimation();
+            yield return new WaitForSeconds(0.5f);
             AerialAttack();
             yield return new WaitForSeconds(aerialAttackInterval);
         }
@@ -87,7 +89,7 @@ public class EnemyController : MonoBehaviour
             groundAttackObject.transform.rotation = attackPivot.rotation;
             groundAttackObject.SetActive(true);
 
-            AttackAnimation();
+            GroundAttackAnimation();
             GroundAttackObject script = groundAttackObject.GetComponent<GroundAttackObject>();
             script.StartMove();
         }
@@ -136,9 +138,15 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    public void AttackAnimation()
+    public void GroundAttackAnimation()
     {
-        animator.SetTrigger(isAttackReady);
-        animator.SetTrigger(isAttack);
+        if(animator==null)return;
+        animator.SetTrigger(isGroundAttack);
+    }
+
+    public void AerialAttackAnimation()
+    {
+        if (animator==null)return;
+        animator.SetTrigger(isAerialAttack);
     }
 }
